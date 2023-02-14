cb00577efed18a305096641fafc1daf8637203b6
CeliktepeMurat <celiktepemurat@gmail.com>
2023-02-14 12:58:10 +0300

    feat: init cargo apps

---
+++ src/hello_rust/target/debug/deps/hello_rust-cdc345886edfe57b.d
@@ -0,0 +1,5 @@
+/Users/murat/Desktop/Rust_Workshop/src/hello_rust/target/debug/deps/hello_rust-cdc345886edfe57b: src/main.rs
+
+/Users/murat/Desktop/Rust_Workshop/src/hello_rust/target/debug/deps/hello_rust-cdc345886edfe57b.d: src/main.rs
+
+src/main.rs:
---
+++ src/hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/Users/murat/Desktop/Rust_Workshop/src/hello_rust/target/debug/hello_rust: /Users/murat/Desktop/Rust_Workshop/src/hello_rust/src/main.rs
