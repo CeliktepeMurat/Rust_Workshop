7c2903203210c955a6d5a38a0dded9864a14b691
CeliktepeMurat <celiktepemurat@gmail.com>
2023-02-15 11:26:37 +0300

    feat: common programming comcepts

--- src/variables/target/debug/deps/variables-ed282611d21feaea.d
+++
@@ -1,5 +0,0 @@
-/Users/murat/Desktop/Rust_Workshop/src/variables/target/debug/deps/variables-ed282611d21feaea.rmeta: src/main.rs
-
-/Users/murat/Desktop/Rust_Workshop/src/variables/target/debug/deps/variables-ed282611d21feaea.d: src/main.rs
-
-src/main.rs:
--- src/variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/murat/Desktop/Rust_Workshop/src/variables/target/debug/variables: /Users/murat/Desktop/Rust_Workshop/src/variables/src/main.rs
